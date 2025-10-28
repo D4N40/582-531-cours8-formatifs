0ca6aec8849391ead79631d6b4d8e5df1de890c4
D4N40 <2396239@cmontmorency.qc.ca>
2025-10-28 14:03:29 -0400

    correction?

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,17 @@ MicroOscSlip<128> monOsc(&Serial);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
   Wire.begin();
   myPbHub.begin();
+  Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int maLectureAngle = myPbHub.analogRead( ANGLE_CHANNEL);
-  int maLectureLight = myPbHub.analogRead( LIGHT_CHANNEL );
   monOsc.sendInt( "/angle" , maLectureAngle);
+  int maLectureLight = myPbHub.analogRead( LIGHT_CHANNEL );
   monOsc.sendInt( "/light" , maLectureLight);
 
   delay(100);
